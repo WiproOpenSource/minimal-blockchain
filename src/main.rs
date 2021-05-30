61218c800f6b244077f6708a6cf8b18328dda234
Tejas Sanap <sanap.tejas@gmail.com>
2021-05-30 14:36:03 +0530

    Added implementationf for transaction and block

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod block;
+mod chain;
+
+use block::{Transaction, Block};
+
 fn main() {
-    println!("Hello, world!");
+    let tran = Transaction::new("001", Some("tejas"));
+    
+    let block = Block::new(Some("0".to_string()), Some(&tran));
+
+    println!("{:#?}", block);
 }
