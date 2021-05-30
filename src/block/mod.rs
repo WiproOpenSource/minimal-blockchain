928d49a8e8d52a9f1f85538ae15a4d83a0787bbf
Tejas Sanap <sanap.tejas@gmail.com>
2021-05-30 23:42:25 +0530

    Commenting out previous code

--- src/block/mod.rs
+++ src/block/mod.rs
@@ -2,47 +2,46 @@ use std::time::SystemTime;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 
-#[derive(Debug, Hash)]
-pub struct Transaction {
-    account: String,
-    timestamp: SystemTime,
-    data: Option<String>
-}
+// #[derive(Debug, Hash)]
+// pub struct Transaction {
+//     account: String,
+//     timestamp: SystemTime,
+//     data: Option<String>
+// }
 
-impl Transaction {
-    pub fn new( account: &str, data: Option<&str>) -> Self
-    {
-        Transaction{
-            account: account.to_string(),
-            timestamp: SystemTime::now(),
-            data: Some(data.unwrap().to_string())
-        }
-    }
-}
+// impl Transaction {
+//     pub fn new( account: &str, data: Option<&str>) -> Self
+//     {
+//         Transaction{
+//             account: account.to_string(),
+//             timestamp: SystemTime::now(),
+//             data: Some(data.unwrap().to_string())
+//         }
+//     }
+// }
 
+// // one block will only have one transaction
+// #[derive(Debug)]
+// pub struct Block<'a> {
+//     timestamp: SystemTime,
+//     transaction: Option<&'a Transaction>,
+//     current_block_hash: Option<String>,
+//     previous_block_hash: Option<String>
+// }
 
-// one block will only have one transaction
-#[derive(Debug)]
-pub struct Block<'a> {
-    timestamp: SystemTime,
-    transaction: Option<&'a Transaction>,
-    current_block_hash: Option<String>,
-    previous_block_hash: Option<String>
-}
+// impl<'t> Block<'t> {
+//     pub fn new(previous_block_hash: Option<String>, transaction: Option<&'t Transaction>) -> Block {
+//         Block {
+//             timestamp: SystemTime::now(),
+//             transaction,
+//             current_block_hash: Block::calculate_hash(transaction.unwrap()),
+//             previous_block_hash
+//         }
+//     }
 
-impl<'t> Block<'t> {
-    pub fn new(previous_block_hash: Option<String>, transaction: Option<&'t Transaction>) -> Block {
-        Block {
-            timestamp: SystemTime::now(),
-            transaction,
-            current_block_hash: Block::calculate_hash(transaction.unwrap()),
-            previous_block_hash
-        }
-    }
-
-    fn calculate_hash(transaction: &Transaction) -> Option<String> {
-        let mut hash = DefaultHasher::new();
-        transaction.hash(&mut hash);
-        return Some(hash.finish().to_string());
-    }
-}
+//     fn calculate_hash(transaction: &Transaction) -> Option<String> {
+//         let mut hash = DefaultHasher::new();
+//         transaction.hash(&mut hash);
+//         return Some(hash.finish().to_string());
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 mod block;
 mod chain;
 
-use block::{Transaction, Block};
+// use block::{Transaction, Block};
 
-fn main() {
-    let tran = Transaction::new("001", Some("tejas"));
-    
-    let block = Block::new(Some("0".to_string()), Some(&tran));
-
-    println!("{:#?}", block);
-}
+// fn main() {
+//     let tran = Transaction::new("001", Some("tejas"));
+//     let block = Block::new(Some("0".to_string()), Some(&tran));
+//     println!("{:#?}", block);
+// }
