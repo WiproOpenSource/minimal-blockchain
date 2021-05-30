e326eba79ed20a7fb4d8c668bfe269ae2ccd28ed
Tejas Sanap <sanap.tejas@gmail.com>
2021-05-31 01:21:11 +0530

    Removing old content from main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 mod block;
 mod chain;
 
-// use block::{Transaction, Block};
-
-// fn main() {
-//     let tran = Transaction::new("001", Some("tejas"));
-//     let block = Block::new(Some("0".to_string()), Some(&tran));
-//     println!("{:#?}", block);
-// }
+fn main()
+{
+    
+}
